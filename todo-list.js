9bf302610c8f9440566579b939751a45aad33da7
Corbin March <marchcorbin@gmail.com>
2020-02-27 15:48:49 -0700

    initial commit

---
+++ todo-list.js
@@ -0,0 +1,18 @@
+class ToDoList {
+  constructor(){
+
+  }
+saveToStorage(){
+  // This is where we will do our local storage
+}
+deleteFromStorage(){
+  // functionality for deleting items from local storage
+}
+updateToDo(){
+  // the comp says "Should update the todos title and urgency"
+}
+updateTask(){
+  // should update a tasks content if it has been completed
+}
+
+}
